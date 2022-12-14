ecc124398ed959e078a284d7bb8df65a856b732b
Ivan Protsko <ivanprotska@MacBook-Pro.local>
2022-12-14 10:28:18 +0800

    Calc game updated

--- src/games/number-calc.js
+++ src/games/number-calc.js
@@ -3,15 +3,28 @@ import GetFeedback from '../components/get-feedback.js';
 import getRandom from '../components/get-random.js';
 import Cli from '../cli.js';
 import GetGreeting from '../components/get-greeting.js';
+import getRandomArbitrary from '../components/get-random-arbitrary.js';
 const BrainCalc = (gameRounds) => {
   const gameType = 'game-type-number';
   GetGreeting();
   const userName = Cli();
   for (let correctAnswerSum = 0; correctAnswerSum < gameRounds; correctAnswerSum += 1) {
-    const numberOne = getRandom();
-    const numberTwo = getRandom();
-    const correctAnswer = numberOne + numberTwo;
-    console.log(`What is sum of numbers: ${numberOne} + ${numberTwo}?`);
+    let numberOne = getRandom();
+    let numberTwo = getRandom();
+    let correctAnswer;
+    const mathOperators = ['+', '-', '*'];
+    const operatorNum = getRandomArbitrary(0, 2);
+    if (operatorNum === 0) {
+      correctAnswer = numberOne + numberTwo;
+    } else if (operatorNum === 1) {
+      correctAnswer = numberOne - numberTwo;
+    } else if (operatorNum === 2) {
+      correctAnswer = numberOne * numberTwo;
+    } else {
+      console.log('Operator is undefined');
+    }
+    console.log(`What is sum of numbers: ${numberOne} ${mathOperators[operatorNum]} ${numberTwo}?`);
+    console.log(correctAnswer);
     const userAnswer = parseInt(readlineSync.question('Your answer: '));
     correctAnswerSum = GetFeedback(
       gameType,
