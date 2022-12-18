f4fbfad243d6d85fb2c8bf1fb32b2fc8b97fb21b
Ivan Protsko <ivanprotska@MacBook-Pro.local>
2022-12-18 10:49:33 +0800

    braind-porgression fixed

--- src/brain-progression.js
+++ src/brain-progression.js
@@ -3,11 +3,10 @@ import gameEngine from './game-engine.js';
 
 const runProgression = (gameRounds) => {
   const description = 'What number is missing in the progression?';
-  const getProgression = (n, lim) => {
-    Array.from({ length: Math.ceil(lim / n) }, (_, i) => (i + 1) * n);
-  };
+  // eslint-disable-next-line max-len
+  const getProgression = (n, lim) => Array.from({ length: Math.ceil(lim / n) }, (_, i) => (i + 1) * n);
   const getRoundData = () => {
-    const arrayLenght = 5;
+    const arrayLenght = 10;
     const randomNumber = getRandomArbitrary(1, 25);
     const progression = getProgression(randomNumber, (randomNumber * arrayLenght));
     const getArrayRandomGap = getRandomArbitrary(0, 4);
